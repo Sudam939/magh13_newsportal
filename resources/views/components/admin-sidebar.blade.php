4ab0303a112e196a58d8db7d6412d3272036017b
Sudam939 <sudamshrestha939@gmail.com>
2025-02-06 21:33:22 +0545

    category create

--- resources/views/components/admin-sidebar.blade.php
+++ resources/views/components/admin-sidebar.blade.php
@@ -1,31 +1,30 @@
 <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-        <a href="{{route('dashboard')}}">
+        <a href="{{ route('dashboard') }}">
             <img alt="image" src="https://codeit.com.np/storage/01JJ6HWH8RP35HYNCEBFXVYZKF.png" class="header-logo" />
         </a>
     </div>
     <ul class="sidebar-menu">
         <li class="menu-header">Main</li>
-        <li class="dropdown active">
-            <a href="{{route('dashboard')}}" class="nav-link"><i
-                    data-feather="monitor"></i><span>Dashboard</span></a>
+        <li class="dropdown {{ Request::routeIs('dashboard') ? 'active' : '' }}">
+            <a href="{{ route('dashboard') }}" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
         </li>
-        <li class="dropdown">
-            <a href="{{route('admin.company.index')}}" class="nav-link"><i class="fas fa-building"></i><span>Company</span></a>
+        <li class="dropdown {{ Request::routeIs('admin.company.*') ? 'active' : '' }}">
+            <a href="{{ route('admin.company.index') }}" class="nav-link"><i
+                    class="fas fa-building"></i><span>Company</span></a>
         </li>
 
-        <li class="dropdown">
-            <a href="{{route('dashboard')}}" class="nav-link"><i
+        <li class="dropdown {{ Request::routeIs('admin.category.*') ? 'active' : '' }}">
+            <a href="{{ route('admin.category.index') }}" class="nav-link"><i
                     class="fas fa-tag"></i><span>Category</span></a>
         </li>
 
         <li class="dropdown">
-            <a href="{{route('dashboard')}}" class="nav-link"><i
-                    class="far fa-newspaper"></i><span>Article</span></a>
+            <a href="{{ route('dashboard') }}" class="nav-link"><i class="far fa-newspaper"></i><span>Article</span></a>
         </li>
 
         <li class="dropdown">
-            <a href="{{route('dashboard')}}" class="nav-link"><i
+            <a href="{{ route('dashboard') }}" class="nav-link"><i
                     class="fas fa-file-image"></i><span>Advertise</span></a>
         </li>
 
