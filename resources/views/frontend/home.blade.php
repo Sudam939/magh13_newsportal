391d4e815ddbbf205daf724eb8b99f753175c7a2
Sudam939 <sudamshrestha939@gmail.com>
2025-02-09 20:39:38 +0545

    minor chnage

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,8 +1,5 @@
 <x-frontend-layout>
     <h1>THis is home page</h1>
-<<<<<<< HEAD
-    <h1>this is added code</h1>
-=======
+
     <h1>this is real code</h1>
->>>>>>> e0a9d9cbc845989f93d4b10032f47ee7c059c6ca
 </x-frontend-layout>
